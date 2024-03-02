a40d30ea84e6d83112cbbc8d64f4611ed821634a
fdemian <fdemian@gmail.com>
2024-03-02 19:55:57 -0300

    Render PDF pages separately.

--- src/PDFViewer/PDFPage.tsx
+++ src/PDFViewer/PDFPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
 interface PDFPageParams {
     pdf: any;
@@ -7,11 +7,27 @@ interface PDFPageParams {
 
 const PDFPage = ({pdf, pageNumber}:PDFPageParams):React.ReactElement => {
 
-    console.clear();
-    console.log(pdf);
-    console.log("+++++++++++");
+    const canvasRef = useRef();  
 
-    return <div>{pageNumber}</div>;
+    const renderPage = useCallback((pageNum:number, pdf) => {
+        pdf && pdf.getPage(pageNum).then(function(page) {
+          const viewport = page.getViewport({scale: 1.5});
+          const canvas = canvasRef.current;
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+          const renderContext = {
+            canvasContext: canvas.getContext('2d'),
+            viewport: viewport
+          };
+          page.render(renderContext);
+        });   
+    }, [pdf]);
+
+    useEffect(() => {
+        renderPage(pageNumber, pdf);
+    }, [])
+
+    return <canvas ref={canvasRef}></canvas>;
 };
 
 export default PDFPage;
--- src/PDFViewer/PDFViewer.tsx
+++ src/PDFViewer/PDFViewer.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect /*, useState, useRef*/ } from 'react';
+import React, { useEffect, useState } from 'react';
+import PDFPage from './PDFPage';
 import * as pdfjsLib from 'pdfjs-dist';
 
 interface PDFVIewerParams {
@@ -7,18 +8,39 @@ interface PDFVIewerParams {
 
 const PDFVIewer = ({url}:PDFVIewerParams): React.ReactElement => {
 
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdfjs.worker.js';
-    //const canvasRef = useRef();  
-    //const [pdfRef, setPdfRef] = useState();
+    pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.mjs';
+    const [pdfRef, setPdfRef] = useState();
     //const [currentPage, setCurrentPage] = useState(1);
-  
+    const [pdfData, setPDFData] = useState(null);
 
     useEffect(() => {
       //const metadata = await this.pdfDocument.getMetadata();
-      console.log("ZERO MINUS ZERO");
+      
+      const loadingTask = pdfjsLib.getDocument(url);
+      loadingTask.promise.then(async (loadedPdf) => {
+        setPdfRef(loadedPdf);
+        const metadata = await loadedPdf.getMetadata();
+        const data = {
+           numPages: loadedPdf.numPages,
+           ...metadata
+        };
+        setPDFData(data);
+      }, function (reason) {
+        console.error(reason);
+      });
     }, [])
+    
+    if(!pdfRef || !pdfData)
+        return <p>Loading....</p>;
 
-    return <h2>{url}</h2>;
+    return(
+    <div>
+        <h2>{url}</h2>
+        <PDFPage pdf={pdfRef} pageNumber={1} />
+        <PDFPage pdf={pdfRef} pageNumber={2} />
+        <PDFPage pdf={pdfRef} pageNumber={3} />
+    </div>
+    );
 }
 
 export default PDFVIewer;
